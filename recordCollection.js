419455dbddb33062234a3d5788234b6ba2501ca2
Yasin Yağmur <yasin.yagmur@gmail.com>
2022-06-01 10:30:33 +0300

    solution

--- recordCollection.js
+++ recordCollection.js
@@ -9,3 +9,41 @@
 // If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
 // If value is an empty string, delete the given prop property from the album.
 // Note: A copy of the recordCollection object is used for the tests.
+
+
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  
+  //! Solution
+  // Only change code below this line
+  function updateRecords(records, id, prop, value) {
+    if ( value === ''){
+      delete records[id][prop];
+    }else if (prop ==='tracks'){
+      records[id][prop] = records[id][prop] || [];
+      records[id][prop].push(value);
+    }else {
+      records[id][prop]=value;
+    }
+    return records;
+  }
+  
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
