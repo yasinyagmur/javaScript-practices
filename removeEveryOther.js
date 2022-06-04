d7b5c18a123a10f62a0c64a63bc84076e146e84a
Yasin Yağmur <yasin.yagmur@gmail.com>
2022-06-04 11:11:25 +0300

    solution

--- removeEveryOther.js
+++ removeEveryOther.js
@@ -5,3 +5,13 @@
 // ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 
 // None of the arrays will be empty, so you don't have to worry about that!
+
+
+//! solution
+function removeEveryOther(arr){
+    for(let i =1; i< arr.length; i++ ){
+      arr.splice(i,1) 
+      }
+      return arr
+  }
+  removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
