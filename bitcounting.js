53c42621c20a07603440ad481a28aadc9db21bd5
yasin yağmur <yasin.yagmur@gmail.com>
2022-06-27 17:58:11 +0300

    solution-2

--- bitcounting.js
+++ bitcounting.js
@@ -6,11 +6,16 @@
 
 //Output
 //["A", "h", "m", "e", "t"]
+//! Soltion 1
+// var countBits = function (n) {
+//   let binary = Array.from(n.toString(2));
+//   console.log(binary);
+//   return binary.map(Number).reduce((a, b) => a + b);
+// };
 
+// countBits(1234);
+//! Soltion 2
 var countBits = function (n) {
-  let binary = Array.from(n.toString(2));
-  console.log(binary);
-  return binary.map(Number).reduce((a, b) => a + b);
-};
+return n.toString(2).split('').filter((x)=>x==1).length
 
-countBits(1234);
+};
