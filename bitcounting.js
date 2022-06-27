dbc37245561ec88eea20a60ec2f2203b5966775d
yasin yağmur <yasin.yagmur@gmail.com>
2022-06-27 17:51:10 +0300

    new question and solution

--- bitcounting.js
+++ bitcounting.js
@@ -1,7 +1,16 @@
-var countBits = function (n) {
-    let binary = Array.from(n.toString(2));
-    console.log(binary);
-    return binary.map(Number).reduce((a, b) => a + b);
-  };
+// https://www.w3schools.com/jsref/jsref_tostring_number.asp   toString() using
 
-countBits(1234)
+//Array.from() metodu bir dizi-benzeri veya gezinilebilir bir nesneden yeni bir Dizi örneği oluşturur.
+
+//Array.from("Ahmet");
+
+//Output
+//["A", "h", "m", "e", "t"]
+
+var countBits = function (n) {
+  let binary = Array.from(n.toString(2));
+  console.log(binary);
+  return binary.map(Number).reduce((a, b) => a + b);
+};
+
+countBits(1234);
