4f05107ce819a4a6fe50f8081de925bbe7314db3
yasin yağmur <yasin.yagmur@gmail.com>
2022-06-27 17:09:05 +0300

    new question

---
+++ Sayhelloworld.js
@@ -0,0 +1,8 @@
+// Write the definition of the function "say" such that calling this:
+
+// say("Hello")("World")
+// returns "Hello World"
+
+var say = function(string1) {
+    return function(string2) {return `${string1} ${string2}`}
+}
