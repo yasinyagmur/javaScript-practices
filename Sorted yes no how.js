67d5c4ed89ab9417eef0fe6614ff766384074558
yasin yağmur <yasin.yagmur@gmail.com>
2022-06-27 17:32:36 +0300

    new question

--- Sorted yes no how.js
+++ Sorted yes no how.js
@@ -5,6 +5,14 @@
 // "no" - otherwise
 // You can assume the array will always be valid, and there will always be one correct answer.
 
+//! Join methods
+// Definition and Usage
+// The join() method returns an array as a string.
+
+// The join() method does not change the original array.
+
+// Any separator can be specified. The default is comma (,)
+
 function isSortedAndHow(array) {
   if ([...array].sort((a, b) => a - b).join("") === array.join("")) {
     return "yes, ascending";
