49cab860e90096911a78fa3c0bf46aadd1c1381a
yasin yağmur <yasin.yagmur@gmail.com>
2022-07-07 23:23:41 +0300

    fixed

--- bitcounting.js
+++ bitcounting.js
@@ -7,15 +7,15 @@
 //Output
 //["A", "h", "m", "e", "t"]
 //! Soltion 1
-var countBits = function (n) {
-  let binary = Array.from(n.toString(2));
-  console.log(binary);
-  return binary.map(Number).reduce((a, b) => a + b);
-};
+// var countBits = function (n) {
+//   let binary = Array.from(n.toString(2));
+//   console.log(binary);
+//   return binary.map(Number).reduce((a, b) => a + b);
+// };
 
 // countBits(1234);
 //! Soltion 2
-// var countBits = function (n) {
-// return n.toString(2).split('').filter((x)=>x==1).length
+var countBits = function (n) {
+return n.toString(2).split('').filter((x)=>x==1).length
 
-// };
+};
